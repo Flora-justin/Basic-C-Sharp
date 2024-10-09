f233eb82235e4145f1d8492bad0b9456518cebe0
Flora-justin <flora.justin@syncfusion.com>
2024-10-09 15:42:56 +0530

    Basic C Sharp Added

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace HelloWorld //it is a room of the project, contains all code modules
+{
+  public class Program
+  {
+    public static void Main(string []args)
+    {
+        Console.WriteLine("Hello World");
+        Console.ReadLine();
+    }
+
+  }
+}
---
+++ SwitchCalculator/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+namespace SwitchCalculator{
+    class Program{
+        public static void Main(string[] args)
+        {Console.WriteLine("enter a number");
+            int num1=int.Parse(Console.ReadLine());
+            Console.WriteLine("enter a number");
+            int num2=int.Parse(Console.ReadLine());
+            Console.WriteLine("press + or - or * or / or %");
+            char symbol=char.Parse(Console.ReadLine());
+            switch(symbol){
+                case '+':
+                {
+                  Console.WriteLine(num1+num2);
+                  break;
+                }
+                case '-':
+                {
+                  Console.WriteLine(num1-num2);
+                   break;
+                }
+                case '*':
+                {
+                  Console.WriteLine(num1*num2);
+                   break;
+                }
+                case '/':
+                {
+                  Console.WriteLine(num1/num2);
+                   break;
+                }
+                case '%':
+                {
+                  Console.WriteLine(num1%num2);
+                   break;
+                }
+                default:
+                {
+                   Console.WriteLine("Operation is invalid"); 
+                    break;
+                }
+                
+            }
+
+
+        }
+
+    }
+}
